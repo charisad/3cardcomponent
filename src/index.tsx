55da498cf16334ea2c508e9ac3f9869fc77acd09
Collins Adom Baffour <adombaffourcollins@gmail.com>
2025-01-15 16:24:49 +0000

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,12 @@ import App from './App';
 import { datadogRum } from '@datadog/browser-rum';
 
 datadogRum.init({
-    applicationId: '9feab793-e8a3-4641-9476-3748fff8e9e2',
-    clientToken: 'pubf80cca8e1d417c86927fc67b2a24fc9b',
+    applicationId: '6bfa7a34-0e44-4465-8cf2-570a8c0aa999',
+    clientToken: 'pubafe2c2f2c959ab251780169a0c328f46',
     // `site` refers to the Datadog site parameter of your organization
     // see https://docs.datadoghq.com/getting_started/site/
     site: 'us5.datadoghq.com',
-    service: 'chariscode',
+    service: 'newrelic',
     env: 'production',
     // Specify a version number to identify the deployed version of your application in Datadog
     // version: '1.0.0',
@@ -20,7 +20,7 @@ datadogRum.init({
     trackResources: true,
     trackLongTasks: true,
     defaultPrivacyLevel: 'mask-user-input',
-}); 
+});
 
 datadogRum.startSessionReplayRecording();
 
