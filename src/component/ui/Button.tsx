f7e707b242883a51d53ae791294a5feef31621b4
Collins Adom Baffour <adombaffourcollins@gmail.com>
2025-01-17 17:03:25 +0000

    Update Button.tsx

--- src/component/ui/Button.tsx
+++ src/component/ui/Button.tsx
@@ -4,14 +4,11 @@ import './styles.css'
 interface Props {
   children?: React.ReactNode;
   onClick: () => void;
-  redirect: string;
 }
 
-const Button: React.FC<Props> = ({children, onClick,redirect}) => {  
-  const handleClick = () => {
-    window.location.href = redirect; // Redirect to the provided URL
-  };
-  return <button onClick={handleClick}> {children} </button>;
+const Button: React.FC<Props> = ({children, onClick}) => {  
+ 
+  return <button onClick={onClick}> {children} </button>;
   
 };
 
